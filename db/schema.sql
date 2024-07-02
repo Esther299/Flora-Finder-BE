2a413b04d8fdc4d5a5c5e2482305576e879d1d5b
OpusLearning <James@opuslearning.co.uk>
2024-07-02 13:19:26 +0100

    updated .env and schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,27 @@
+
+DROP TABLE IF EXISTS UserCollection;
+DROP TABLE IF EXISTS UserAccount;
+
+-- Create UserAccount table
 CREATE TABLE UserAccount (
     username VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    dateStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    avatar VARCHAR(255) DEFAULT 'default_avatar.png'
+    dateStamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    avatar VARCHAR(255)
 );
 
+-- Create UserCollection table
 CREATE TABLE UserCollection (
-    uniqueSerialID UUID PRIMARY KEY,
+    uniqueSerialID VARCHAR(36) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     speciesID INT NOT NULL,
     speciesName VARCHAR(255) NOT NULL,
-    geoTag GEOMETRY NOT NULL,
-    matchScore DECIMAL(5, 2) DEFAULT 0,
-    dateCollected TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    image VARCHAR(255) NOT NULL,
-    speciesFamily VARCHAR(255) NOT NULL,
+    geoTag VARCHAR(255),
+    matchScore DECIMAL(5,2),
+    dateCollected DATETIME,
+    image VARCHAR(255),
+    speciesFamily VARCHAR(255),
     FOREIGN KEY (username) REFERENCES UserAccount(username)
 );
-
-
-
---SELECT ua.username, ua.name, ua.email, ua.dateStamp, ua.avatar, COUNT(uc.speciesID) AS totalSpecies
---FROM UserAccount ua
---LEFT JOIN UserCollection uc ON ua.username = uc.username
---GROUP BY ua.username, ua.name, ua.email, ua.dateStamp, ua.avatar;
