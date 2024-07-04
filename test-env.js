080a51a837832c1ef5b61959b0cfc4951d12e753
Esther299 <esthergc_2@hotmail.com>
2024-07-04 12:37:37 +0200

    Removed unused files

--- test-env.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-console.log("Database User:", process.env.DB_USER);
-console.log("Database Host:", process.env.DB_HOST);
-console.log("Database Name:", process.env.DB_NAME);
-console.log("Database Password:", process.env.DB_PASSWORD);
-console.log("Database Port:", process.env.DB_PORT);
