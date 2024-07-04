b6e15d659f816196dbbc64d8371195905677d1aa
Esther299 <esthergc_2@hotmail.com>
2024-07-04 13:50:43 +0200

    Last update to git ignore

--- db/connection.js
+++ db/connection.js
@@ -10,10 +10,4 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
-// checking dot env is up and running
-// console.log("Database User:", process.env.DB_USER);
-// console.log("Database Host:", process.env.DB_HOST);
-// console.log("Database Name:", process.env.DB_NAME);
-// console.log("Database Password:", process.env.DB_PASSWORD);
-// console.log("Database Port:", process.env.DB_PORT);
 module.exports = pool;
