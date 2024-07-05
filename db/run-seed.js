b29e62bbc60f7b3358bd6a217cae947f3def78b1
Esther299 <esthergc_2@hotmail.com>
2024-07-05 15:53:02 +0200

    Updated run-seed file

--- db/run-seed.js
+++ db/run-seed.js
@@ -1,8 +1,15 @@
 const seedDatabase = require("./seed.js");
 const pool = require("./connection.js");
 
-const runSeed = () => {
-  return seedDatabase().then(() => pool.end());
+const runSeed = async () => {
+  try {
+    await seedDatabase();
+    console.log("Database seeding completed successfully!");
+  } catch (error) {
+    console.error("Error seeding the database:", error);
+  } finally {
+    pool.end();
+  }
 };
 
 runSeed();
